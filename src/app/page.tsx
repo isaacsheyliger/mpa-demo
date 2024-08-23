f65968ecd243e580fbf6d64d740ba63882a2cc71
Jonathan Isaacs-Heyliger <heyligerjon@gmail.com>
2024-08-23 17:12:08 -0400

    change animation interval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
             if (nextButtonRef.current) {
                 nextButtonRef.current.click();
             }
-        }, 10000);
+        }, 15000);
 
         return () => {
             clearInterval(interval);
